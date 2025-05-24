8a05491a8dc55aba59b7b10366306ed03fb1a958
Lucas Alves de Melo Lis <Lucas.l@IMARUILESTE.DOMINIOLOCAL>
2025-05-24 10:32:37 -0300

    atualização do schemma

--- src/router/basesRouter.js
+++ src/router/basesRouter.js
@@ -1,17 +1,9 @@
 import express from 'express'
-import multer from 'multer'
-import path from 'path'
 import { atualBaseController } from '../controller/atualBaseController.js'
-import { uploadBaseController } from '../controller/uploadBaseController.js'
 
 
 const router  = express.Router()
 
-
-const upload = multer({ storage })
-
-
-router.post('/updateBase', upload.single('file'), uploadBaseController);
 router.get('/atual', atualBaseController)
 
 
